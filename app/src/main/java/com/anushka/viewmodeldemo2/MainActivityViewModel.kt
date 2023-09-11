e860627efe1bba20693dc8406be1c853bac26aac
preslybenoit <okebenoithub@gmail.com>
2023-09-11 23:50:08 +0100

    View Model Challenge

---
+++ app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -0,0 +1,15 @@
+package com.anushka.viewmodeldemo2
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    private var total = 0
+
+    fun getTotal():Int {
+        return total
+    }
+
+    fun setTotal(input:Int) {
+        total += input
+    }
+}
